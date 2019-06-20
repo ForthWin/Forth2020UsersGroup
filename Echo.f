83962aeb7be80fce54871a4fe98182672d96a350
ForthWin <51237047+ForthWin@users.noreply.github.com>
2019-06-20 14:09:48 -0400

    Update Echo.f

--- Echo.f
+++ Echo.f
@@ -26,9 +26,6 @@ variable pos-Y
    Point 
 ;
 
-
-
-
 \ ===============================================================
 create  monde dim-monde allot
 create  monde-buf dim-monde allot
@@ -52,41 +49,41 @@ variable etat
 
 \ 9 voisins possibles
 : est ( n -- )
-1 + dim-monde mod *monde @ + c@ last-bit ;
+	1 + dim-monde mod *monde @ + c@ last-bit ;
 
 : ouest ( n -- )
-1 - dim-monde mod *monde @ + c@ last-bit ;
+	1 - dim-monde mod *monde @ + c@ last-bit ;
 
 : nord ( n -- )
-largeur-monde - dim-monde mod *monde @ + c@ last-bit ;
+	largeur-monde - dim-monde mod *monde @ + c@ last-bit ;
 
 : sud ( n -- )
-largeur-monde + dim-monde mod *monde @ + c@ last-bit ;
+	largeur-monde + dim-monde mod *monde @ + c@ last-bit ;
 
 : nord-est ( n -- )
-largeur-monde - 1 + dim-monde mod *monde @ + c@ last-bit ;
+	largeur-monde - 1 + dim-monde mod *monde @ + c@ last-bit ;
 
 : nord-ouest ( n -- )
-largeur-monde - 1 - dim-monde mod *monde @ + c@ last-bit ;
+	largeur-monde - 1 - dim-monde mod *monde @ + c@ last-bit ;
 
 : sud-est ( n -- )
-largeur-monde + 1 + dim-monde mod *monde @ + c@ last-bit ;
+	largeur-monde + 1 + dim-monde mod *monde @ + c@ last-bit ;
 
 : sud-ouest ( n -- )
-largeur-monde + 1 - dim-monde mod *monde @ + c@ last-bit ;
+	largeur-monde + 1 - dim-monde mod *monde @ + c@ last-bit ;
 
 \ voisinage de moore
 
 : moore ( n -- ) \ returne le nombre de voisins
 
-dup nord-ouest
-swap dup nord
-swap dup nord-est
-swap dup est
-swap dup sud-est
-swap dup sud
-swap dup sud-ouest
-swap ouest
+	dup nord-ouest
+	swap dup nord
+	swap dup nord-est
+	swap dup est
+	swap dup sud-est
+	swap dup sud
+	swap dup sud-ouest
+	swap ouest
 ;
 
 : somme-moore
@@ -96,48 +93,47 @@ moore
 
 \ regles de conway avec echo
 : conway-echo1
-dup
-centre etat c! \ l'état est 1 ou 0, vivant ou mort
-somme-moore total c!
-total @ 3 = if 3 etat c! else
-total @ 2 = if ( reste pareil ) else
-0 etat c!
-then then
-etat c@
+	dup
+	centre etat c! \ l'état est 1 ou 0, vivant ou mort
+	somme-moore total c!
+	total @ 3 = if 3 etat c! else
+	total @ 2 = if ( reste pareil ) else
+	0 etat c!
+	then then
+	etat c@
 ;
 
 
 : conway-echo
-dup
-centre etat c! \ l'état est 1 ou 0, vivant ou mort
-somme-moore total c!
+	dup
+	centre etat c! \ l'état est 1 ou 0, vivant ou mort
+	somme-moore total c!
 
-etat @ 0 = total @ 3 = and if 1 etat c! else
-etat @ 1 = total @ 3 = and if 3 etat c! else
-etat @ 1 = total @ 2 = and if 3 etat c! else
-etat @ 1 = if 2 etat c! else
-0 etat c!
-then then then then
-etat c@
+	etat @ 0 = total @ 3 = and if 1 etat c! else
+	etat @ 1 = total @ 3 = and if 3 etat c! else
+	etat @ 1 = total @ 2 = and if 3 etat c! else
+	etat @ 1 = if 2 etat c! else
+	0 etat c!
+	then then then then
+	etat c@
 ;
 \ =========================================
 
 : initialisation ( -- ) \ monde aléatoire
-*monde @ dim-monde 0 fill
-dim-monde 0 do 8 random
-1 = if 1 *monde @ i + c! then loop
+	*monde @ dim-monde 0 fill
+	dim-monde 0 do 8 random
+	1 = if 1 *monde @ i + c! then loop
 ;
 
 : regenere-monde
-dim-monde 0 do i conway-echo *monde-buf @ i + c! loop
-*monde tampon 1 cells move
-*monde-buf *monde 1 cells move
-tampon *monde-buf 1 cells move
+	dim-monde 0 do i conway-echo *monde-buf @ i + c! loop
+	*monde tampon 1 cells move
+	*monde-buf *monde 1 cells move
+	tampon *monde-buf 1 cells move
 ;
 
-
 : dessine-monde ( -- )
-largeur-monde 0 do
+	largeur-monde 0 do
 	hauteur-monde 0 do
 		largeur-monde i * j + *monde @ + c@ 1 = if
 				j i yellow pixel else
@@ -147,21 +143,20 @@ largeur-monde 0 do
 				j i green pixel else
 				j i black pixel
 
-then then then
-loop loop
-
+	then then then
+	loop loop
 ;
 
 : run
-initialisation
-Dessin
-dessine-monde
-do
-dessine-monde
-regenere-monde
-view
-10 pause
-loop
+	initialisation
+	Dessin
+	dessine-monde
+	do
+	dessine-monde
+	regenere-monde
+	view
+	10 pause
+	loop
 ;
 
 run 
