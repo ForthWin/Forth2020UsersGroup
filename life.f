422a3256b219ea3c542983c0ae7bb9b4d9dfafee
ForthWin <51237047+ForthWin@users.noreply.github.com>
2019-06-20 14:01:09 -0400

    Update life.f

--- life.f
+++ life.f
@@ -26,9 +26,6 @@ variable pos-Y
    Point 
 ;
 
-
-
-
 \ ===================================
 
 create  monde dim-monde allot
@@ -49,96 +46,92 @@ variable etat
 ;
 \ 9 voisins possibles
 : est ( n -- )
-1 + dim-monde mod *monde @ + c@ ;
+	1 + dim-monde mod *monde @ + c@ ;
 
 : ouest ( n -- )
-1 - dim-monde mod *monde @ + c@ ;
+	1 - dim-monde mod *monde @ + c@ ;
 
 : nord ( n -- )
-largeur-monde - dim-monde mod *monde @ + c@ ;
+	largeur-monde - dim-monde mod *monde @ + c@ ;
 
 : sud ( n -- )
-largeur-monde + dim-monde mod *monde @ + c@ ;
+	largeur-monde + dim-monde mod *monde @ + c@ ;
 
 : nord-est ( n -- )
-largeur-monde - 1 + dim-monde mod *monde @ + c@ ;
+	largeur-monde - 1 + dim-monde mod *monde @ + c@ ;
 
 : nord-ouest ( n -- )
-largeur-monde - 1 - dim-monde mod *monde @ + c@ ;
+	largeur-monde - 1 - dim-monde mod *monde @ + c@ ;
 
 : sud-est ( n -- )
-largeur-monde + 1 + dim-monde mod *monde @ + c@ ;
+	largeur-monde + 1 + dim-monde mod *monde @ + c@ ;
 
 : sud-ouest ( n -- )
-largeur-monde + 1 - dim-monde mod *monde @ + c@ ;
+	largeur-monde + 1 - dim-monde mod *monde @ + c@ ;
 
 \ voisinage de moore
 
 : somme-moore ( n -- ) \ returne le nombre de voisins
-dup nord-ouest
-swap dup nord
-swap dup nord-est
-swap dup est
-swap dup sud-est
-swap dup sud
-swap dup sud-ouest
-swap ouest
-+ + + + + + +
+	dup nord-ouest
+	swap dup nord
+	swap dup nord-est
+	swap dup est
+	swap dup sud-est
+	swap dup sud
+	swap dup sud-ouest
+	swap ouest
+	+ + + + + + +
 ;
 
 \ regles de conway
 : conway
-dup
-centre etat c!
-somme-moore total c!
+	dup
+	centre etat c!
+	somme-moore total c!
 
-total @ 3 = if 1 etat c! else
-total @ 2 = if ( reste pareil ) else
-0 etat c!
-then then
-etat c@
+	total @ 3 = if 1 etat c! else
+	total @ 2 = if ( reste pareil ) else
+	0 etat c!
+	then then
+	etat c@
 ;
 
 \ =========================================
 
 : initialisation ( -- ) \ monde aléatoire
-*monde @ dim-monde 0 fill
-dim-monde 0 do 10 random
-1 = if 1 *monde @ i + c! then loop
+	*monde @ dim-monde 0 fill
+	dim-monde 0 do 10 random
+	1 = if 1 *monde @ i + c! then loop
 ;
 
 : regenere-monde
-dim-monde 0 do i conway *monde-buf @ i + c! loop
-*monde tampon 1 cells move
-*monde-buf *monde 1 cells move
-tampon *monde-buf 1 cells move
+	dim-monde 0 do i conway *monde-buf @ i + c! loop
+	*monde tampon 1 cells move
+	*monde-buf *monde 1 cells move
+	tampon *monde-buf 1 cells move
 ;
 
-
 : dessine-monde ( -- )
-largeur-monde 0 do
+	largeur-monde 0 do
 	hauteur-monde 0 do
 		largeur-monde i * j + *monde @ + c@ 1 = if
                 j i green pixel else
 				j i black pixel
 
-then
-loop loop
+	then
+	loop loop
 ;
 
-
-
-
 : run
-initialisation
-Dessin
-dessine-monde
-do
-dessine-monde
-regenere-monde
-view
-10 pause
-loop
+	initialisation
+	Dessin
+	dessine-monde
+	do
+		dessine-monde
+		regenere-monde
+		view
+		10 pause
+	loop
 ;
 
 run 
