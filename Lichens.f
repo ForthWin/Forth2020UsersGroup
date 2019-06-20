8d21f102b18bf0ea3dd6c44c26da97b6f1255199
ForthWin <51237047+ForthWin@users.noreply.github.com>
2019-06-20 14:09:52 -0400

    Update Lichens.f

--- Lichens.f
+++ Lichens.f
@@ -48,100 +48,100 @@ variable etat \ etat de la cellule
 ;
 \ 9 voisins possibles
 : est ( n -- )
-1 + dim-monde mod *monde @ + c@ ;
+	1 + dim-monde mod *monde @ + c@ ;
 
 : ouest ( n -- )
-1 - dim-monde mod *monde @ + c@ ;
+	1 - dim-monde mod *monde @ + c@ ;
 
 : nord ( n -- )
-largeur-monde - dim-monde mod *monde @ + c@ ;
+	largeur-monde - dim-monde mod *monde @ + c@ ;
 
 : sud ( n -- )
-largeur-monde + dim-monde mod *monde @ + c@ ;
+	largeur-monde + dim-monde mod *monde @ + c@ ;
 
 : nord-est ( n -- )
-largeur-monde - 1 + dim-monde mod *monde @ + c@ ;
+	largeur-monde - 1 + dim-monde mod *monde @ + c@ ;
 
 : nord-ouest ( n -- )
-largeur-monde - 1 - dim-monde mod *monde @ + c@ ;
+	largeur-monde - 1 - dim-monde mod *monde @ + c@ ;
 
 : sud-est ( n -- )
-largeur-monde + 1 + dim-monde mod *monde @ + c@ ;
+	largeur-monde + 1 + dim-monde mod *monde @ + c@ ;
 
 : sud-ouest ( n -- )
-largeur-monde + 1 - dim-monde mod *monde @ + c@ ;
+	largeur-monde + 1 - dim-monde mod *monde @ + c@ ;
 
 \ voisinage de moore
 
 : moore ( n -- ) \ returne le nombre de voisins
 
-dup nord-ouest
-swap dup nord
-swap dup nord-est
-swap dup est
-swap dup sud-est
-swap dup sud
-swap dup sud-ouest
-swap ouest
+	dup nord-ouest
+	swap dup nord
+	swap dup nord-est
+	swap dup est
+	swap dup sud-est
+	swap dup sud
+	swap dup sud-ouest
+	swap ouest
 ;
 
 : somme-moore
-moore
-+ + + + + + +
+	moore
+	+ + + + + + +
 ;
 
 \ regles lichens
 : lichens
-dup
-centre etat c!
-somme-moore total c!
+	dup
+	centre etat c!
+	somme-moore total c!
 
-total @ 3 = if 1 etat c! ELSE
-total @ 4 = if 0 etat c! ELSE
-total @ 7 = if 1 etat c! ELSE
-total @ 8 = if 1 etat c!
-then then then then
-etat c@
+	total @ 3 = if 1 etat c! ELSE
+	total @ 4 = if 0 etat c! ELSE
+	total @ 7 = if 1 etat c! ELSE
+	total @ 8 = if 1 etat c!
+	then then then then
+	etat c@
 ;
 
 \ =========================================
 
 : initialisation ( -- ) \ monde aléatoire
-*monde @ dim-monde 0 fill
-dim-monde 0 do 100 random
-1 = if 1 *monde @ i + c! then loop
+	*monde @ dim-monde 0 fill
+	dim-monde 0 do 100 random
+	1 = if 1 *monde @ i + c! then loop
 ;
 
 : regenere-monde
-dim-monde 0 do i lichens *monde-buf @ i + c! loop
-*monde tampon 1 cells move
-*monde-buf *monde 1 cells move
-tampon *monde-buf 1 cells move
+	dim-monde 0 do i lichens *monde-buf @ i + c! loop
+	*monde tampon 1 cells move
+	*monde-buf *monde 1 cells move
+	tampon *monde-buf 1 cells move
 ;
 
 
 : dessine-monde ( -- )
-largeur-monde 0 do
+	largeur-monde 0 do
 	hauteur-monde 0 do
 		largeur-monde i * j + *monde @ + c@ 1 = if
 			j i green pixel else
 				j i black pixel
 
-then
-loop loop
+	then
+	loop loop
 
 ;
 
 : run
-initialisation
-Dessin
-dessine-monde
-do
-dessine-monde
-regenere-monde
-view
-10 pause
-loop
+	initialisation
+	Dessin
+	dessine-monde
+	do
+		dessine-monde
+		regenere-monde
+		view
+		10 pause
+	loop
 ;
 
 run 
