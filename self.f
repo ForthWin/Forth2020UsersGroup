4f8168360c578c4b7791d8879ee44aff6c62a78d
ForthWin <51237047+ForthWin@users.noreply.github.com>
2020-03-26 10:17:40 -0400

    Create self.f

---
+++ self.f
@@ -0,0 +1,5 @@
+\ self is a computer program which takes no input and produces a copy of its 
+\ own source code as its only output. The standard terms for these programs 
+\ are "self-replicating programs".
+
+: self s" : self s over 9 type 34 emit space 2dup type 34 emit 9 /string type ;" over 9 type 34 emit space 2dup type 34 emit 9 /string type ;
