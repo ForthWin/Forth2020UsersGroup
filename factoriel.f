aa5b5cc5f85ad1388a24ec02202b14b514a42a65
Michel Jean <michelj@hotmail.com>
2021-12-25 14:57:44 -0500

    Update factoriel.f

--- factoriel.f
+++ factoriel.f
@@ -2,46 +2,40 @@
 \ Michel Jean - December 2021
 
 create result 10000 cells allot \ Size of the result
-variable result_size 
-variable carry
-variable product
-variable x
-variable number
+1 value result_size 
+2 value x
 
-: calcul ( -- ) 
-    0 carry !
-    result_size @ 0 do
-    i cells result + @ x @  * carry @ + product !
-    product @ 10 mod result i cells + !
-    product @ 10 / carry !
+: calcul { carry product } 
+    result_size 0 do
+    i cells result + @ x * carry + -> product
+    product 10 mod result i cells + !
+    product 10 / -> carry
     loop 
     
     begin
-    carry @ 0 > while
-    carry @ 10 mod result result_size @ cells + !
-    carry @ 10 / carry !
-    result_size @ 1+ result_size !
+    carry 0 > while
+    carry 10 mod result result_size cells + !
+    carry 10 / -> carry
+    result_size 1+ to result_size
     repeat
 ;
 : output ( -- )
-    ." The factorial of " number ? ." is : " cr
-    result_size @ 1- result_size !
+    cr
+    result_size 1- to result_size
     begin
-    result_size @ 0 >= while
-    result_size @ cells result + ? 
-    result_size @ 1- result_size !
+    result_size 0 >= while
+    result_size cells result + ? 
+    result_size 1- to result_size
+    result_size 30 mod 0 = if cr then \ number by line 
     repeat
 ;
-: factorial ( -- )
-    number !
+: factorial { number }
     1 result 0 cells + !
-    1 result_size !
-    2 x !
     
     begin 
-     x @ number @ <= while 
- calcul
-    x @ 1+ x !
+     x number <= while 
+ 0 0 calcul
+    x 1+ to x
      repeat
     output 
 ;
