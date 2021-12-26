8517ef39c09d894f0a4829562ec33b57ad839695
Michel Jean <michelj@hotmail.com>
2021-12-26 10:22:46 -0500

    Update factoriel.f

--- factoriel.f
+++ factoriel.f
@@ -1,42 +1,49 @@
 \ Program to calculate big factorial
 \ Michel Jean - December 2021
+\ Version without local variable
 
 create result 10000 cells allot \ Size of the result
-0 value result_size 
-0 value x
+variable result_size 
+variable carry
+variable product
+variable x
+variable number
 
-: calcul { carry product } 
-    result_size 0 do
-    i cells result + @ x * carry + -> product
-    product 10 mod result i cells + !
-    product 10 / -> carry
+: calcul ( -- ) 
+    0 carry !
+    result_size @ 0 do
+    i cells result + @ x @  * carry @ + product !
+    product @ 10 mod result i cells + !
+    product @ 10 / carry !
     loop 
     
     begin
-    carry 0 > while
-    carry 10 mod result result_size cells + !
-    carry 10 / -> carry
-    result_size 1+ to result_size
+    carry @ 0 > while
+    carry @ 10 mod result result_size @ cells + !
+    carry @ 10 / carry !
+    result_size @ 1+ result_size !
     repeat
 ;
 : output ( -- )
-    cr
-    result_size 1- to result_size
+    ." The factorial of " number ? ." is : " cr
+    result_size @ 1- result_size !
     begin
-    result_size 0 >= while
-    result_size cells result + ? 
-    result_size 1- to result_size
+    result_size @ 0 >= while
+    result_size @ cells result + ? 
+    result_size @ 1- result_size !
     result_size 30 mod 0 = if cr then \ number by line 
     repeat
 ;
-: factorial { number }
+: factorial ( -- )
+    number !
     1 result 0 cells + !
-    1 value result_size 
-    2 value x
-begin 
-     x number <= while 
-    0 0 calcul
-    x 1+ to x
+    1 result_size !
+    2 x !
+    
+    begin 
+     x @ number @ <= while 
+ calcul
+    x @ 1+ x !
      repeat
     output 
 ;
